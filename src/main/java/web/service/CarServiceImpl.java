54887aa1b75a711006318c2dc444a2049d6b9438
Nailac <12gadzik98@gmail.com>
2021-11-28 22:35:33 +0300

    final commit

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -9,7 +9,7 @@ public class CarServiceImpl implements CarService {
     @Override
     public List<Car> getCars(List<Car> carList, int count) {
         if (count > 0 && count < 5) {
-            return carList.subList(0, count);   //Нужно сделать здесь коллекцию машин cars
+            return carList.subList(0, count);
         } else {
             return carList;
         }
